0195715715d68a2d6aa88e220deee51316030314
Kevin Lange <lange7@zaphod.acm.uiuc.edu>
2010-10-27 16:19:47 -0500

    Scripts directory

--- install.sh
+++ install.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 echo -e "\e[1mInstalling (or updating) ksdots...\e[0m"
 
+echo -e "\e[1;32mInstalling rc files...\e[0m"
 current_path=$(pwd)
 cd ~/
 for i in  $current_path/home/*; do
 	echo -n "Installing $i -> "
 	j=`echo $i | sed "s/.*home\//\./"`
 	echo "~/$j"
+	rm -r $j
+	echo "ln -f -s $i $j"
+	ln -f -s $i $j
+done
+
+echo -e "\e[1;32mInstalling scripts...\e[0m"
+cd ~/bin
+for i in $current_path/scripts/*; do
+	echo -n "Installing $i -> "
+	j=`echo $i | sed "s/.*home\//\./"`
+	echo "~/bin/$j"
 	rm -r $j
 	echo "ln -f -s $i $j"
 	ln -f -s $i $j
